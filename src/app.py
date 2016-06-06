760bbea2d302d34b6f5f7b201e5b1bd7c649fb52
kanairen <turutekab@gmail.com>
2016-06-06 10:01:49 +0900

    modify app.py中で用いられる定値をconfig.pyに移動

--- src/app.py
+++ src/app.py
@@ -8,73 +8,55 @@ from config import *
 
 
 class MultiTimerApp(rumps.App):
-    APP_TITLE = ""
-
-    UPDATE_VIEW_INTERVAL = 0.1
-
-    MENU_ITEM_ADD_TIMER = 'Add Timer'
-    MENU_ITEM_REMOVE_TIMER = 'Remove Timer'
-
-    WINDOW_TITLE_MESSAGE = 'Timer Title'
-    WINDOW_TIME_MESSAGE = 'Timer Length'
-    WINDOW_DIM = (320, 20)
-
-    TIME_EXAMPLE = '00:00:00'
-    TIME_VIEW_FORMAT = '{}[ {} ] '
-    TIME_FINISHED_FORMAT = 'Timer \'{}\' finished.'
-
-    INVALID_TIME_FORMAT_MESSAGE = 'Timer length should be input like \'HH:MM:SS\''
-
     def __init__(self):
-        super(MultiTimerApp, self).__init__(self.APP_TITLE,
+        super(MultiTimerApp, self).__init__(APP_TITLE,
                                             icon=PATH_RES_IMG_TIME)
 
-        self.menu = [MultiTimerApp.MENU_ITEM_ADD_TIMER,
-                     MultiTimerApp.MENU_ITEM_REMOVE_TIMER]
+        self.menu = [MENU_ITEM_ADD_TIMER, MENU_ITEM_REMOVE_TIMER]
         self.timers = []
 
     @rumps.timer(UPDATE_VIEW_INTERVAL)
     def _update_view(self, _):
         if len(self.timers) == 0:
-            self.title = MultiTimerApp.APP_TITLE
+            self.title = APP_TITLE
         else:
             title = ''
             for timer in self.timers:
-                title += MultiTimerApp.TIME_VIEW_FORMAT.format(
+                title += TIME_VIEW_FORMAT.format(
                     timer.title.split('[')[0],
                     MultiTimerApp.sec_to_hms(timer.current_sec))
             self.title = title
 
     @rumps.clicked(MENU_ITEM_ADD_TIMER)
     def _add_timer(self, _):
-        res_title = rumps.Window(title=MultiTimerApp.WINDOW_TITLE_MESSAGE,
+        res_title = rumps.Window(title=WINDOW_TITLE_MESSAGE,
                                  message='',
                                  default_text='',
                                  cancel=True,
-                                 dimensions=MultiTimerApp.WINDOW_DIM).run()
+                                 dimensions=WINDOW_DIM).run()
         if not res_title.clicked:
             return
 
         sec = None
         while sec is None:
-            res_sec = rumps.Window(title=MultiTimerApp.WINDOW_TIME_MESSAGE,
+            res_sec = rumps.Window(title=WINDOW_TIME_MESSAGE,
                                    message='',
-                                   default_text=MultiTimerApp.TIME_EXAMPLE,
+                                   default_text=TIME_EXAMPLE,
                                    cancel=True,
-                                   dimensions=MultiTimerApp.WINDOW_DIM).run()
+                                   dimensions=WINDOW_DIM).run()
             if not res_sec.clicked:
                 return
 
             try:
                 sec = MultiTimerApp.hms_to_sec(res_sec.text)
             except TypeError:
-                rumps.alert(MultiTimerApp.INVALID_TIME_FORMAT_MESSAGE)
+                rumps.alert(INVALID_TIME_FORMAT_MESSAGE)
 
         title = res_title.text
 
         def on_finish():
             self._update_view(None)
-            rumps.alert(MultiTimerApp.TIME_FINISHED_FORMAT.format(title))
+            rumps.alert(TIME_FINISHED_FORMAT.format(title))
             self._stop_timer(self._get_timer(title))
 
         self.timers.append(Timer(title, sec, on_finish))
@@ -82,7 +64,7 @@ class MultiTimerApp(rumps.App):
                                      callback=self._switch_timer,
                                      icon=PATH_RES_IMG_PLAY))
 
-        remove_menu = self.menu[MultiTimerApp.MENU_ITEM_REMOVE_TIMER]
+        remove_menu = self.menu[MENU_ITEM_REMOVE_TIMER]
         remove_menu.set_callback(lambda: None)
         remove_menu.add(rumps.MenuItem(title, callback=self._remove_timer))
 
@@ -93,10 +75,10 @@ class MultiTimerApp(rumps.App):
                 timer.stop()
                 self.timers.remove(timer)
                 self.menu.pop(timer.title)
-                self.menu[MultiTimerApp.MENU_ITEM_REMOVE_TIMER].pop(timer.title)
+                self.menu[MENU_ITEM_REMOVE_TIMER].pop(timer.title)
                 break
         if len(self.timers) == 0:
-            self.menu[MultiTimerApp.MENU_ITEM_REMOVE_TIMER].set_callback(None)
+            self.menu[MENU_ITEM_REMOVE_TIMER].set_callback(None)
 
     def _get_timer(self, title):
         for timer in self.timers:
--- src/config.py
+++ src/config.py
@@ -3,6 +3,7 @@
 
 import os
 
+#PATH
 PATH = os.path.join(os.path.dirname(__file__), '..')
 PATH_RES = os.path.join(PATH, 'res')
 
@@ -12,3 +13,22 @@ PATH_RES_IMG_PAUSE = os.path.join(PATH_RES_IMG, 'pause.png')
 PATH_RES_IMG_PLAY = os.path.join(PATH_RES_IMG, 'play.png')
 PATH_RES_IMG_STOP = os.path.join(PATH_RES_IMG, 'stop.png')
 PATH_RES_IMG_TIME = os.path.join(PATH_RES_IMG, 'time.png')
+
+# App Config
+APP_TITLE = ""
+
+UPDATE_VIEW_INTERVAL = 0.1
+
+MENU_ITEM_ADD_TIMER = 'Add Timer'
+MENU_ITEM_REMOVE_TIMER = 'Remove Timer'
+
+WINDOW_TITLE_MESSAGE = 'Timer Title'
+WINDOW_TIME_MESSAGE = 'Timer Length'
+WINDOW_DIM = (320, 20)
+
+TIME_EXAMPLE = '00:00:00'
+TIME_VIEW_FORMAT = '{}[ {} ] '
+TIME_FINISHED_FORMAT = 'Timer \'{}\' finished.'
+
+INVALID_TIME_FORMAT_MESSAGE = 'Timer length should be input like \'HH:MM:SS\''
+
